1b09eceba12ef8d88a410c1b20564c0516f4decc
Camilo <jcam.qvision@gmail.com>
2022-05-22 22:04:47 -0500

    binding agregado

--- app/src/main/java/com/codeludo/myloginapplication/LoginFragment.kt
+++ app/src/main/java/com/codeludo/myloginapplication/LoginFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.codeludo.myloginapplication.databinding.ActivityMainBinding
+import com.codeludo.myloginapplication.databinding.FragmentLoginBinding
+import com.codeludo.myloginapplication.databinding.FragmentLoginBindingImpl
 
 class LoginFragment : Fragment() {
 
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(
+            inflater,
+            R.layout.fragment_login,
+            container,
+            false)
+        return binding.root
     }
 
 }
